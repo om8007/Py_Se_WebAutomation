6ed730e6f92830b87f6c7007081f18396496a6b3
OM PRAKASH <32073787+omprakash8007@users.noreply.github.com>
2019-12-11 01:57:35 +0530

    fixed some code in group-contact-saver

--- group-contact-saver.py
+++ group-contact-saver.py
@@ -16,21 +16,16 @@ driver = webdriver.Firefox(executable_path=r'Z:\python\driver\geckodriver.exe') 
 
 driver.get("https:/web.whatsapp.com/")
 wait = WebDriverWait(driver,600)
-group = '" <target group name as in your whatsapp> "'           #Enter group name within double quotes
+group = '"Labs @IITRPR JOC2019"'           #Enter group name within double quotes
 
 x_arg = '//span[contains(@title,'+ group + ')]'
 group = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
 group.click()
 
-three_dots_arg='//*[@id="main"]/header/div[3]/div/div[3]/div/span'
-dot_menu = wait.until(EC.presence_of_element_located((By.XPATH, three_dots_arg)))
-dot_menu.click()
+header = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/header/div[2]/div[1]/div/span')
+header.click()
 
-group_info_arg = '//*[@id="main"]/header/div[3]/div/div[3]/span/div/ul/li[1]/div'
-group_info = wait.until(EC.presence_of_element_located((By.XPATH, group_info_arg)))
-group_info.click()
+members = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div[1]/div[5]/div[1]/div/div/div[1]/span')
+members.click()
 
-members_arg = '//*[@id="app"]/div/div/div[2]/div[3]/span/div/span/div/div/div[1]/div[5]/div[1]/div/div/div[1]/span'
-members_list = wait.until(EC.presence_of_all_elements_located((By.XPATH,members_arg)))
-members_list.click()
-
+# TO DO => members list not populating
