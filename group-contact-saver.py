67b4ab2bf4ae4e0fbf29ee9d68783552556cb6f1
OM PRAKASH <32073787+omprakash8007@users.noreply.github.com>
2020-01-11 21:43:16 +0530

    some contacts not get appended to list

--- group-contact-saver.py
+++ group-contact-saver.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-
+import csv
 
 driver = webdriver.Firefox(executable_path=r'Z:\python\driver\geckodriver.exe')  #local path to your firefox driver
 
@@ -30,8 +30,8 @@ driver.execute_script("document.body.style.zoom='60%'")
 
 # part = '"participants"'
 # participant_arg = '//span[contains(text(),'+ part +')]'
-time.sleep(5)
-participant_list = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div[1]/div[5]/div[1]/div/div/div[1]').click()
+# time.sleep(10)
+participant_list = wait.until(EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div[1]/div[5]/div[1]/div/div/div[1]'))).click()
 # wait.until(EC.presence_of_element_located((By.XPATH, x_arg))).click()
 
 
@@ -48,9 +48,24 @@ participant_box = driver.find_element_by_xpath('/html/body/div[1]/div/span[2]/di
 #     time.sleep(2)
 # print(nums)
 
-divs = participant_box.find_elements_by_class_name('_2WP9Q')
-for div in divs:
-    numbs = div.find_elements_by_class_name('_1ovWX')
-    names = div.find_elements_by_class_name('_3VvbK')
-    for a,b in zip(numbs, names):
-        print(a.text,b.text)
+list = []
+for i in range(26):
+    divs = participant_box.find_elements_by_class_name('_2WP9Q')
+    for div in divs:
+        nums = div.find_elements_by_class_name('_1ovWX')
+        names = div.find_elements_by_class_name('_3VvbK')
+        for num,name in zip(nums, names):
+            # i = num.text
+            if (num.text,name.text) not in list:
+                list.append((num.text,name.text))
+    driver.execute_script("arguments[0].scrollBy(0,422);", participant_box)
+    time.sleep(2)  
+    
+driver.refresh()
+print(len(list))
+
+with open('group.csv', mode='w+',newline='',encoding="utf-8") as group_file:
+    csv_writer = csv.writer(group_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)    
+
+    for l in list:
+        csv_writer.writerow(l)
